88d923dd6d1baba7d75678725afd592cdef9f901
nuttawatso <nuttawat.so.60@ubu.ac.th>
2020-09-30 16:05:49 +0700

    add template profile 30092563

--- mean/src/app/showposts/showposts.component.spec.ts
+++ mean/src/app/showposts/showposts.component.spec.ts
@@ -19,7 +19,7 @@ describe('ShowpostsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
